0430f7303b0cff997eff3a768bf978b07c604223
Nathanaeladii <m26414104@john.petra.ac.id>
2016-09-11 23:06:09 +0700

    Hari2 Coba1

---
+++ 11092016.py
@@ -0,0 +1,62 @@
+ 482  mkdir 100916
+  483  cd 100916
+  484  cd
+  485  ls
+  486  cd tugas01tos
+  487  ls
+  488  cp git.txt 100916
+  489  cd
+  490  cd 100916
+  491  ls
+  492  cd
+  493  cd tugas01tos
+  494  mv git.txt 100916
+  495  cd
+  496  cd 100916
+  497  ls
+  498  cd
+  499  pwd
+  500  mv /home/tugas01tos/git.txt /home/100916
+  501  mv /home/tugas01tos/git.txt /home/100916/git1.txt
+  502  cd tugas01tos
+  503  cp git.txt 100916
+  504  cp git.txt /home/100916
+  505  cd
+  506  cp tugas01tos 100916
+  507  100916
+  508  cd 100916
+  509  ls
+  510  cd
+  511  rmdir 100916
+  512  ls
+  513  cd tugas01tos
+  514  ls
+  515  cat > coba
+  516  vi coba
+  517  cd tugas01
+  518  ls
+  519  cd
+  520  cd tugas01tos
+  521  grep ^s coba
+  522  pwd
+  523  cat > coba
+  524  grep ^s coba
+  525  grep i$ coba
+  526  nano cobased.txt
+  527  ps
+  528  pstree
+  529  top
+  530  htop
+  531  kill
+  532  free
+  533  jobs
+  534  ipconfig
+  535  hostname darmawan
+  536  touch apa.txt
+  537  ls
+  538  strings -f apa.txt
+  539  vi apa.txt
+  540  strings -f apa.txt
+  541  nano apa.txt
+  542  strings -a apa.txt
+  543  history
