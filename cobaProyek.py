b0ffa2eb82585e4e9a33156fff83b0f2222385f7
Nathanaeladii <m26414104@john.petra.ac.id>
2016-12-02 22:08:53 +0700

    Insert Quiz

---
+++ cobaProyek.py
@@ -0,0 +1,73 @@
+ass AdminInsert(Music):
+    def __init__(self, root, title):
+        self.root = root
+        self.root.title(title)
+        self.root.geometry('300x420+450+150')
+        self.imagebackground = Image.open('image\gbackground.jpeg')
+        self.displaybackground= ImageTk.PhotoImage(self.imagebackground)
+        self.labelbackground = Label(self.root, image=self.displaybackground).place(x=-5, y=-5)
+        self.labelspace = Label(self.root, bg='black').pack()
+        self.Menu_bar = Menu(self.root)
+        self.Menu_bar.add_command(label="Update", command=self.updatequiz)
+        self.Menu_bar.add_command(label="Logout", command=self.logoutquiz)
+        self.root.config(menu=self.Menu_bar)
+        self.labeljudulAdmin = Label(self.root, text='ADMIN PANEL', fg='blue', font=("Helvetica", 16), bg='black').pack()
+        self.labeljudulInsert = Label(self.root, text='INSERT YOUR QUESTION, ANSWER AND HINT', fg='blue',font=("Helvetica", 8), bg='black').pack()
+        self.labelspace = Label(self.root, bg='black').pack()
+        self.labelquestion = Label(self.root, text='Question: ', bg='black', fg='white').pack()
+        self.stringquestion = StringVar()
+        self.entryquestion = Entry(self.root, textvariable=self.stringquestion).pack()
+        self.labelanswer = Label(self.root, text='Answer ', bg='black', fg='white').pack()
+        self.stringanswer = StringVar()
+        self.entrynaswer = Entry(self.root, textvariable=self.stringanswer).pack()
+        self.labelhint = Label(self.root, text='Hint ', bg='black', fg='white').pack()
+        self.stringhint = StringVar()
+        self.entryhint = Entry(self.root, textvariable=self.stringhint).pack()
+        self.labelspace = Label(self.root, bg='black').pack()
+        self.buttoninsert = Button(self.root, text='INSERT', command=self.insertquiz, width='16').pack()
+        Music.PlayMusicAdmin(self)
+
+        self.imageplay = Image.open('image\gplay.png')
+        self.displayplay = ImageTk.PhotoImage(self.imageplay)
+        self.buttonplay = Button(self.root, command=self.play, image=self.displayplay).place(x=70, y=355)
+        self.imageresume = Image.open('image\gresume.png')
+        self.displayresume = ImageTk.PhotoImage(self.imageresume)
+        self.buttonresume = Button(self.root, command=self.resume, image=self.displayresume).place(x=110, y=355)
+        self.imagepause = Image.open('image\gpause.png')
+        self.displaypause = ImageTk.PhotoImage(self.imagepause)
+        self.buttonpause = Button(self.root, command=self.pause, image=self.displaypause).place(x=150, y=355)
+        self.imagestop = Image.open('image\gstop.png')
+        self.displaystop = ImageTk.PhotoImage(self.imagestop)
+        self.buttonstop = Button(self.root, command=self.stop, image=self.displaystop).place(x=190, y=355)
+    def play(self):
+        Music.PlayMusicAdmin(self)
+    def resume(self):
+        Music.UnpauseMusic(self)
+    def pause(self):
+        Music.PauseMusic(self)
+    def stop(self):
+        Music.StopMusic(self)
+    def insertquiz(self):
+        tempquestion = self.stringquestion.get()
+        tempanswer = self.stringanswer.get()
+        temphint = self.stringhint.get()
+        if tempquestion == '' or tempanswer == '' or temphint == '' :
+            tkMessageBox.showwarning(title="Error !", message="Empty Question/Answer/Hint")
+        else:
+            c.execute("INSERT INTO quizadmin (question, answer, hint) VALUES (?,?,?)", (tempquestion, tempanswer, temphint,))
+            conn.commit()
+            tkMessageBox.showwarning(title="INSERT SUCCESS", message="Insert Success")
+            self.root.destroy()
+            self.insquiz = Tk()
+            self.app = AdminInsert(self.insquiz, 'Insert Quiz')
+            self.insquiz.deiconify()
+    def updatequiz(self):
+        self.root.destroy()
+        self.upquiz = Tk()
+        self.app = SearchUpdateQuiz(self.upquiz, 'Update Quiz')
+        self.upquiz.deiconify()
+    def logoutquiz(self):
+        self.root.destroy()
+        self.logout = Tk()
+        self.app = Login(self.logout, 'Login')
+        self.logout.deiconify()
